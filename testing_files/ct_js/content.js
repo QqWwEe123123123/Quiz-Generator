2045042eb408ab9a57b4497aad8f51893c04c0b1
QqWwEe123123123 <64291296+QqWwEe123123123@users.noreply.github.com>
2020-11-08 01:28:45 -0500

    back to front

--- testing_files/ct_js/content.js
+++ testing_files/ct_js/content.js
@@ -1,1 +1,26 @@
-document.querySelector('#hplogo').classList.add('spinspinspin');
+// document.querySelector('#hplogo').classList.add('spinspinspin');
+
+const first = document.createElement('button');
+first.innerText = "SET DATA";
+first.id = "first";
+
+const second = document.createElement('button');
+second.innerText = "BACKEND";
+second.id = "second";
+
+document.querySelector('body').appendChild(first);
+document.querySelector('body').appendChild(second);
+
+first.addEventListener('click', () => {
+    chrome.storage.local.set({"password": "123"});
+    console.log("I SET DATA");
+})
+
+second.addEventListener('click', () => {
+    chrome.runtime.sendMessage({message: "yo check the storage"});
+    console.log("I SENT THE MESSAGE");
+})
+
+chrome.runtime.onMessage.addEventListener((request, sender, sendResponse) =>{
+    console.log(request.message);
+})
