1e46437a2fda45e3fac71a22a9d0250dd0b7548b
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2020-11-08 03:23:13 -0500

    revert

--- working input reader/content.js
+++ working input reader/content.js
@@ -5,13 +5,5 @@ document.addEventListener('keypress', logKey);
 let stringStorage = '';
 
 function logKey (keyPressed){
-    // simulate keyboard non-exit signal
-    if (keyPressed.key != 0){
-        stringStorage += keyPressed.key;
-    }
-    // simulate mouse exit signal
-    else{
-        console.log(stringStorage);
-        stringStorage = '';
-    };
+        console.log(keyPressed.key);
 }
