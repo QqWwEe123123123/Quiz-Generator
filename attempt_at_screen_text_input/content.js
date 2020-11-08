b731daddbd11488866c75c82ec6a25011543000b
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2020-11-08 07:16:00 -0500

    not working comms code

---
+++ attempt_at_screen_text_input/content.js
@@ -0,0 +1,45 @@
+
+let stringStorage = '';
+
+function logKey (keyPressed){
+    stringStorage += keyPressed.key;
+    console.log(keyPressed.key);
+}
+
+// sender
+function messageSender(){
+
+    let params = {
+        active : true,
+        currentWindow : true,
+        WindowType : "popup"
+    }
+    let returning = {
+        rtn = stringStorage
+    }
+
+    chrome.tabs.query(params, gotTabs);
+
+    function gotTabs(tabs){
+        console.log("Sending message:" + returning.rtn)
+        chrome.tabs.sendMessage(tabs[0].id, returning);
+    }
+
+}
+
+// activates only when called with a message
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse){
+
+    console.log("Recieved message: " + message);
+
+    if (message == "record"){
+        document.addEventListener('keypress', logKey);
+    }
+    else if (message == "stop"){
+        document.removeEventListener('keypress', logKey);
+        messageSender();
+    }
+
+}
