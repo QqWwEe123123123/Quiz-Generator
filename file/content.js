df6212d145999c7897c97127ca03033cff26d4df
QqWwEe123123123 <64291296+QqWwEe123123123@users.noreply.github.com>
2020-11-08 09:54:43 -0500

    merge winston code to file

--- file/content.js
+++ file/content.js
@@ -1,7 +1,44 @@
-console.log("Content active");
 
-document.addEventListener('keypress', logKey);
+let stringStorage = '';
 
 function logKey (keyPressed){
-        console.log(keyPressed.key);
+    stringStorage += keyPressed.key;
+    console.log(keyPressed.key);
+}
+
+// sender
+function messageSender(sender){
+
+    let params = {
+        active : true,
+        currentWindow : true,
+        WindowType : "popup"
+    }
+    let returning = {
+        rtn = stringStorage
+    }
+
+    chrome.tabs.query(params, gotTabs);
+
+    function gotTabs(tabs){
+        console.log("Sending message:" + returning.rtn);
+        chrome.tabs.sendMessage(sender.tab.id, returning);
+    }
+
+}
+
+// activates only when called with a message
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse){
+
+    console.log("Recieved message: " + message);
+
+    if (message == "record"){
+        document.addEventListener('keypress', logKey);
+    }
+    else if (message == "stop"){
+        document.removeEventListener('keypress', logKey);
+        messageSender(sender);
+    }
 }
