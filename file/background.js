660be13a45db216c838d503b551fa1d1bffaf5ce
QqWwEe123123123 <64291296+QqWwEe123123123@users.noreply.github.com>
2020-11-08 03:13:59 -0500

    button pressed

--- file/background.js
+++ file/background.js
@@ -1,8 +1,13 @@
-console.log("background running");
+document.getElementById("play").addEventListener('click',play);
+document.getElementById("record").addEventListener('click',record);
+document.getElementById("stop").addEventListener('click',stop);
 
-chrome.browserAction.onClicked.addListener(buttonClicked)
-console.log("AA");
-
-function buttonClicked(){
-    console.log("button pressed");
+function play(){ // play button pressed
+    console.log("play pressed");
+}
+function record(){ // record button pressed
+    console.log("record pressed");
+}
+function stop(){ // stop button pressed
+    console.log("stop pressed");
 }
