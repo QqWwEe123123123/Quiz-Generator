b20f52c70ff97bba4e210ee167541c3f1a54ab2e
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2020-11-08 03:12:20 -0500

    update

--- working input reader/content.js
+++ working input reader/content.js
@@ -2,6 +2,16 @@ console.log("Content active");
 
 document.addEventListener('keypress', logKey);
 
+let stringStorage = '';
+
 function logKey (keyPressed){
-    console.log(keyPressed.key);
+    // simulate keyboard non-exit signal
+    if (keyPressed.key != 0){
+        stringStorage += keyPressed.key;
+    }
+    // simulate mouse exit signal
+    else{
+        console.log(stringStorage);
+        stringStorage = '';
+    };
 }
